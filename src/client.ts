62bef06769f57213d0a79a4549900c7609caa4e9
10-abhi <abhishek81936@gmail.com>
2026-03-14 13:37:34 +0530

    added multiple device/socket of single user handling logic

--- src/client.ts
+++ src/client.ts
@@ -2,10 +2,24 @@ import WebSocket from "ws";
 
 export type UserId = string;
 
+// "all" — all devices for a user receive events (default)
+// "latest" — new connection added , and closes all previous ones for that user
+const DEVICE_POLICY = process.env.DEVICE_POLICY || "all";
+
 const clients : Map<UserId , Set<WebSocket>> = new Map();
 
 export function addClient(userId : UserId , ws : WebSocket):void{
    let userSocket = clients.get(userId);
+   
+    //logic for the user already present
+   if (DEVICE_POLICY === "latest" && userSocket) {
+       for (const existingWs of userSocket) {
+           existingWs.close(1000, "Replaced by new connection");
+       }
+       userSocket = new Set<WebSocket>();
+       clients.set(userId, userSocket);
+   }
+
    if(!userSocket){
     userSocket = new Set<WebSocket>();
     clients.set(userId, userSocket);
